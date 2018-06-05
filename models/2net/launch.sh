eef67ce774a3a530045212c73736bc7c9c169c97
Lincoln Thurlow <lincoln@isi.edu>
2018-06-05 11:23:55 -0700

    appease gocd -/

--- models/2net/launch.sh
+++ models/2net/launch.sh
@@ -20,21 +20,21 @@ phase "Fetching walrus"
   fi
 
 phase "Building"
-  bin/echo "clearing out any artifacts from previous runs"
-  usr/bin/sudo -E rvn destroy
-  bin/echo "building system"
-  usr/bin/sudo -E rvn build
+  /bin/echo "clearing out any artifacts from previous runs"
+  /usr/bin/sudo -E rvn destroy
+  /bin/echo "building system"
+  /usr/bin/sudo -E rvn build
 
 phase "Deploying"
-  bin/echo "launching vms"
-  usr/bin/sudo -E rvn deploy
-  bin/echo "waiting for vms to come on network"
-  usr/bin/sudo -E rvn pingwait control walrus nimbus n0 n1
+  /bin/echo "launching vms"
+  /usr/bin/sudo -E rvn deploy
+  /bin/echo "waiting for vms to come on network"
+  /usr/bin/sudo -E rvn pingwait control walrus nimbus n0 n1
 
 phase "Configuring"
-  usr/bin/sudo -E rvn configure
+  /usr/bin/sudo -E rvn configure
 
 phase "Testing"
-  bin/echo "launching tests"
-  usr/bin/sudo -E rvn ansible walrus config/run_tests.yml
+  /bin/echo "launching tests"
+  /usr/bin/sudo -E rvn ansible walrus config/run_tests.yml
   wtf -collector=`rvn ip walrus` watch config/files/walrus/tests.json
